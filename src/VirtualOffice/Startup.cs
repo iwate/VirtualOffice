37ca1944562689a98c4127bb4ecc38836425dd5f
iwate <iwatesoftware@live.jp>
2020-05-01 23:37:28 +0900

    add status error page

--- src/VirtualOffice/Startup.cs
+++ src/VirtualOffice/Startup.cs
@@ -53,7 +53,9 @@ namespace VirtualOffice
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
             app.UseHttpsRedirection();
+            app.UseStatusCodePages();
             app.UseStaticFiles();
 
             app.UseRouting();
